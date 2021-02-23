d6dcb0be7eca2dfb8c8a80bf6a381ac42e93bc11
xjqh <79516712+xjqh@users.noreply.github.com>
2021-02-23 14:26:56 +0000

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+         System.out.println("hello world1");
+         System.out.println("hello world2");
     }
 }
